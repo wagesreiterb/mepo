cb1376f95289bfc0b5ca523cf366f526b81791ff
Wagesreiter Bernhard <bernhard.wagesreiter@a1telekom.at>
2018-12-05 10:25:26 +0100

    added static directory

--- mepo/settings.py
+++ mepo/settings.py
@@ -104,15 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -120,3 +115,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '~/static'

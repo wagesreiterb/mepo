2cba5904657957fb8b2b01ff26014030e5cae592
wagesreiterb <42894347+wagesreiterb@users.noreply.github.com>
2018-12-10 11:29:52 +0100

    Update settings.py

--- mepo/settings.py
+++ mepo/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     'portal',
 ]
 
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = os.environ.get('STATIC_URL')
+#STATIC_URL = os.environ.get('STATIC_URL')
 #STATIC_URL = 'http://mepo-static-nfv.openshift-poc.at.inside/static/'
 #STATIC_URL = '/static/'
 #STATIC_ROOT = 'static'
